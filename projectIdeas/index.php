951923977452c6c0f6d8b89af385eb750853b1a0
Andrew Combs <andrewcombs13@gmail.com>
2017-05-01 15:30:46 -0400

    Actually add project list
    Just copied from Keep list. Still need to modify and write descriptions

--- projectIdeas/index.php
+++ projectIdeas/index.php
@@ -2,40 +2,276 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title> Games | AndrewCombs13.com </title>
+	<title> Project Ideas | AndrewCombs13.com </title>
 	
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<ul>
 		<li>
-			<a href="drip/"><h3>Drip</h3></a>
-			<a href="drip/"><img alt="drip screenshot" title="drip screenshot" src="drip/img.jpg"></a>
+			<a href="drip/"><h3>Ultrasonic radar</h3></a>
 			<div class="copy">
-				<p>A simple javascript application that paints colored circles to the screen.</p>
+				<p></p>
 			</div>
 		</li>
 		<li>
-			<a href="ancientClock/"><h3>Ancient Clock</h3></a>
-			<a href="ancientClock/"><img alt="ancient clock screenshot" title="ancient clock screenshot" src="ancientClock/img.jpg"></a>
+			<a href="drip/"><h3>Money to scholarships</h3></a>
 			<div class="copy">
-				<p>Displays the current time in Ancient, a language from the Stargate fictional universe.</p>
+				<p></p>
 			</div>
 		</li>
 		<li>
-			<a href="blotch/"><h3>Blotch</h3></a>
-			<a href="blotch/"><img alt="blotch screenshot" title="blotch screenshot" src="blotch/img.jpg"></a>
+			<a href="drip/"><h3>Word search solver with ocr</h3></a>
 			<div class="copy">
-				<p>Very similar to Drip, except circles are placed by mouse click rather than randomly.</p>
+				<p></p>
 			</div>
 		</li>
 		<li>
-			<a href="back/"><h3>Back</h3></a>
-			<a href="back/"><img alt="back screenshot" title="back screenshot" src="back/img.jpg"></a>
+			<a href="drip/"><h3>Detachable proximity sensor</h3></a>
 			<div class="copy">
-				<p>Changes the color of the screen background using the same random color generator used by drip.</p>
+				<p></p>
 			</div>
 		</li>
+		<li>
+			<a href="drip/"><h3>Smart medicine cabinet</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Phone smart charger (stops charging after full charge)</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>VHS Data Tape Backup</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Program to order wifi preference on win 10 graphically</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Distributed temperature sensor thermostat w/ window directions</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>ESP 8266 one-to-many audio streaming</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>surround sound with cheap speakers, perhaps wireless with above</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>monitor that dims when you look away</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Neural network voice teacher</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Autoranging hand towel dispenser</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Hex grid game of life, other game of life thing</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Dystopian RTS</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Disposable chat thing</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Smart phone</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Coil gun</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Secure Phone -Analog Encryption -Recording -voice modulation</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>microcontroller SDR</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Text based adventure w/. multiplayer</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>6 dof vr fps</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>AdChaff</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>a3bu Oscope/Logic Analyser/function generator</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>bluetooth numeric keypad</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>morse code speaker from loud printer</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Age of Empires second screen info mod (#villagers, military breakdown, etc)</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Arduino Yahtzee (Good complex example project)</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Age of Umpires (credit Paul)</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Reddit search engine</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Kerbal space program simulator</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Amazon Deals RSS Feed Scraper</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Physical WiFi pc remote</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Breathing simulator (vr?)</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Vector Spacewar / take apart CRT</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Lunch with a pro</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>cbord cardinal cash chart</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Better delete history</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Desktop cup holder (w/ drink dispenser?)</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Ad detect radio</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Suing machine</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>Better motion lights</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li>
+		<li>
+			<a href="drip/"><h3>"Am I wasting my time" website (yes)</h3></a>
+			<div class="copy">
+				<p></p>
+			</div>
+		</li
 	</ul>
 </body>
 </html>
