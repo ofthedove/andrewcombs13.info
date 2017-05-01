faf27582fe0ba40d6049ed6bb92fb6e75d1b5863
Andrew Combs <andrewcombs13@gmail.com>
2017-05-01 17:16:56 -0400

    Re-add resume download link
    I must have done this without checking it in in the past, so I re-did it

--- resume/resume.php
+++ resume/resume.php
@@ -16,7 +16,8 @@
 		<div class="container">
 			<h1>Andrew Combs</h1>
 			<ul>
-				<li class="last-item"><a href="resume.php" class="active">Resume</a></li
+				<li><a href="resume.php" class="active">Resume</a></li
+				><li class="last-item"><a href="resume.pdf">Download PDF</a></li
 				><!--<li class="last-item"><a href="portfolio.php">Portfolio</a></li
 				>--><!--<li class="last-item"><a href="contact.php">Contact</a></li>-->
 			</ul>
