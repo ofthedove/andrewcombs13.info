7cb15ef1a898c6a85a0a042b37c054822b2e0495
Andrew Combs <andrewcombs13@gmail.com>
2017-05-01 16:53:42 -0400

    Actually write the project list
    Version 1 Call it stable and ship it. I'm done with this for today

--- projectIdeas/index.php
+++ projectIdeas/index.php
@@ -7,271 +7,250 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+   <h1>Project Ideas</h1>
+   <h2>A bunch of ideas for projects, originally for inspiration at hackathons</h2>
+   <h2>It's a mix of hardware and software projects</h2>
 	<ul>
 		<li>
-			<a href="drip/"><h3>Ultrasonic radar</h3></a>
+			<h3>Ultrasonic radar</h3>
 			<div class="copy">
-				<p></p>
+				<p>Use a stepper to spin an ultrasonic sensor around. Read the distance values and display on a radar-like screen.</p>
+            <p>I used this one as a final project for a class.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Money to scholarships</h3></a>
+			<h3>Money to scholarships</h3>
 			<div class="copy">
-				<p></p>
+				<p>Make a browser extension that converts large sums of money to the number of scholarships that money could fund. Have settings for what school and full ride vs full scholarship, etc.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Word search solver with ocr</h3></a>
+			<h3>Word search solver with ocr</h3>
 			<div class="copy">
-				<p></p>
+				<p>Solve word-search puzzles. This would be really fun from an algorithmic point of view. Hard mode - use OCR to run it on paper puzzles</p>
+            <p>I was on a team that implemented this at a hackathon, link <a href="https://devpost.com/software/lexitrek">here</a></p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Detachable proximity sensor</h3></a>
+			<h3>Detachable proximity sensor</h3>
 			<div class="copy">
-				<p></p>
+				<p>Build a motion detector and RF transmitter that you can set somewhere. Also build a device with an RF receiver that will vibrate/buzz/light up when the motion detector 'sees' someone. Never have someone sneak up behind you again!</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Smart medicine cabinet</h3></a>
+			<h3>Smart medicine cabinet</h3>
 			<div class="copy">
-				<p></p>
+				<p>Mdeicne cabinet with a magic mirror that helps you keep track of when you take your medice. Never worry about taking to much medicine while you're sick again! Could also interface with the FDA's online database to warn people of possible interactions and show potential side effects, etc.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Phone smart charger (stops charging after full charge)</h3></a>
+			<h3>Phone smart charger (stops charging after full charge)</h3>
 			<div class="copy">
-				<p></p>
+				<p>App on your phone would track charge level, alert an Arduino/Raspberry Pi when your phone battery is full and shut off the charger with a relay. Could even track charge time and set to hit full charge right before you wake up.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>VHS Data Tape Backup</h3></a>
+			<h3>VHS Data Tape Backup</h3>
 			<div class="copy">
-				<p></p>
+				<p>Store computer data to VHS tapes.</p>
+            <p>Worked with a team to implement this at a hackathon. One of the most fun projects I've done at a hackathon. Details <a href="https://devpost.com/software/video-home-storage-odumbn">here</a></p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Program to order wifi preference on win 10 graphically</h3></a>
+			<h3>Program to order wifi preference on win 10 graphically</h3>
 			<div class="copy">
-				<p></p>
+				<p>Currently Windows 10 only allows you to change what wifi networks are most preferred through powershell. Would be easy to make a GUI for it, and very useful.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Distributed temperature sensor thermostat w/ window directions</h3></a>
+			<h3>Distributed temperature sensor thermostat w/ window directions</h3>
 			<div class="copy">
-				<p></p>
+				<p>One of the biggest issues with thermostats is that they only sense temperature in one place in your house. Use a network of wifi-connected temperature sensors to get an average temperature, and build a thermostat off that. Bonus points: have a sensor outside and have the thermostat reccomend opening windows when more efficient that running HVAC. Maybe even integrate with online pollution index.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>ESP 8266 one-to-many audio streaming</h3></a>
+			<h3>ESP 8266 one-to-many audio streaming</h3>
 			<div class="copy">
-				<p></p>
+				<p>Implement audio streaming over bare UDP on ESP 8266s. Would be challenging to learn about stream control, networking, error correction, synchronization, etc.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>surround sound with cheap speakers, perhaps wireless with above</h3></a>
+			<h3>Surround sound with cheap speakers, perhaps wireless with above</h3>
 			<div class="copy">
-				<p></p>
+				<p>Turn 3 sets of stereo speakers into 6 channel surround sound using math and electronics. Difficulty level: hard.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>monitor that dims when you look away</h3></a>
+			<h3>Monitor that dims when you look away</h3>
 			<div class="copy">
-				<p></p>
+				<p>Use a webcam and facial recognition (probably with OpenCV) to detect whether you're looking at your monitor. Dim your monitor when you're looking away to save power and ward off curious co-workers</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Neural network voice teacher</h3></a>
+			<h3>Neural network voice teacher</h3>
 			<div class="copy">
-				<p></p>
+				<p><a href="https://rocksmith.ubisoft.com/rocksmith/en-us/home/">This</a> but with a mic for voice</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Autoranging hand towel dispenser</h3></a>
+			<h3>Autoranging hand towel dispenser</h3>
 			<div class="copy">
-				<p></p>
+				<p>Have you noticed how a lot of towel dispensers dispense towels even when no one is nearby? Use a basic AI to allow towel dispensers to automatically change their motion sensor sensitivity.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Hex grid game of life, other game of life thing</h3></a>
+			<h3>Hex grid game of life, other game of life thing</h3>
 			<div class="copy">
-				<p></p>
+				<p>Game of life, except hexes instead of squares</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Dystopian RTS</h3></a>
+			<h3>Dystopian RTS</h3>
 			<div class="copy">
-				<p></p>
+				<p>RTS, but dystopian.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Disposable chat thing</h3></a>
+			<h3>Disposable chat thing</h3>
 			<div class="copy">
-				<p></p>
+				<p>Chat where you only keep your username as long as you're using it, and can change it at any time. Lots of anonymity.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Smart phone</h3></a>
+			<h3>Smart phone</h3>
 			<div class="copy">
-				<p></p>
+				<p>Build a smart phone with an arduino/raspberry pi.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Coil gun</h3></a>
+			<h3>Coil gun</h3>
 			<div class="copy">
-				<p></p>
+				<p>Capacitors and magnet wire. Move nails _really_ fast</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Secure Phone -Analog Encryption -Recording -voice modulation</h3></a>
+			<h3>Secure Phone -Analog Encryption -Recording -voice modulation</h3>
 			<div class="copy">
-				<p></p>
+				<p>Build a phone with an arduino (or raspberry pi) that adds all kinds of security features, low and high tech.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>microcontroller SDR</h3></a>
+			<h3>Microcontroller SDR</h3>
 			<div class="copy">
-				<p></p>
+				<p>Low tech SDR using fast RAM, a binary counter, a micro, some adders, and PWM DAC.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Text based adventure w/. multiplayer</h3></a>
+			<h3>Text based adventure w/. multiplayer</h3>
 			<div class="copy">
-				<p></p>
+				<p>Text based adventure, except multiplayer</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>6 dof vr fps</h3></a>
+			<h3>6 dof vr fps</h3>
 			<div class="copy">
-				<p></p>
+				<p>First person shooter set in space with full 6 degrees of freedom. Bonus points for accurate orbital mechanics. Basically Shattered Horizons.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>AdChaff</h3></a>
+			<h3>AdChaff</h3>
 			<div class="copy">
-				<p></p>
+				<p>Have a pi that you log into with Google and Facebook and such, then it constantly visits random websites and clicks on ads. Make your online tracking data useless. Bonus points for simiple AI stuff, like turning off to simulate "sleep." Similar too, but different from, <a href="https://adnauseam.io/">AdNauseam</a></p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>a3bu Oscope/Logic Analyser/function generator</h3></a>
+			<h3>XMega a3bu Oscope/Logic Analyser/function generator</h3>
 			<div class="copy">
-				<p></p>
+				<p>Build a terrible little Oscope with a microcontroller. Wouldn't be very good, but it would be cheap and useful for simple Arduino projects</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>bluetooth numeric keypad</h3></a>
+			<h3>Bluetooth numeric keypad</h3>
 			<div class="copy">
-				<p></p>
+				<p>Wireless bluetooth number pad. Surprisingly difficult to find one you can buy.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>morse code speaker from loud printer</h3></a>
+			<h3>Morse code speaker from loud printer</h3>
 			<div class="copy">
-				<p></p>
+				<p>You know how some printers make loud noises as the print head moves back and forth? Write a program that, when given an input string, converts it to Morse code then generates a pdf that makes the printer move to sound out the morse code.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Age of Empires second screen info mod (#villagers, military breakdown, etc)</h3></a>
+			<h3>Age of Empires second screen info mod (#villagers, military breakdown, etc)</h3>
 			<div class="copy">
-				<p></p>
+				<p>Sow details about game statistics in a second window. Pretty straightforward.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Arduino Yahtzee (Good complex example project)</h3></a>
+			<h3>Arduino Yahtzee (Good complex example project)</h3>
 			<div class="copy">
-				<p></p>
+				<p>Implement the game Yahtzee on an Arduino. Would require a lot of knowledge of coding and, if done carefully, could cover almost every feature of object oriented programming and Arduino hardware.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Age of Umpires (credit Paul)</h3></a>
+			<h3>Age of Umpires<h3>
 			<div class="copy">
-				<p></p>
+				<p>Age of Empires satire based on baseball. Credit goes to my little brother for this idea.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Reddit search engine</h3></a>
+			<h3>Reddit search engine</h3>
 			<div class="copy">
-				<p></p>
+				<p>Build an "advanced search" for Reddit. Allow you to search keywords, titles, usernames, etc. and filter by sub, date, poster, etc.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Kerbal space program simulator</h3></a>
+			<h3>Kerbal space program simulator</h3>
 			<div class="copy">
-				<p></p>
+				<p>Simulate playing the classic simulator! Press a button faster to make more things blow up!</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Amazon Deals RSS Feed Scraper</h3></a>
+			<h3>Amazon Deals RSS Feed Scraper</h3>
 			<div class="copy">
-				<p></p>
+				<p>Amazon publishes an RSS feed of sales. Build a program that reads the feed and searches for specified keywords. Bonus points for filtering by cost, user rating etc. For example, you could specify that you want a 128GB SSD for less than $50 and it would alert you as soon as one came up.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Physical WiFi pc remote</h3></a>
+			<h3>Physical WiFi pc remote</h3>
 			<div class="copy">
-				<p></p>
+				<p>Build a programmable remote for your PC that worked over WiFi. Power it with a Pi, ESP 8266, or Onion Omega. Communicate with VNC or have a small program on the PC that executed AutoHotKey scripts based on input from the remote.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Breathing simulator (vr?)</h3></a>
+			<h3>Breathing simulator (vr?)</h3>
 			<div class="copy">
-				<p></p>
+				<p>Breathing is actually really hard! Take the place of someones autonomic nervous system while they pursue a variety of activities. Can you keep their blood appropriately oxygenated based only on their heart rate and blood C02 levels?</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Vector Spacewar / take apart CRT</h3></a>
+			<h3>Vector Spacewar / take apart CRT</h3>
 			<div class="copy">
-				<p></p>
+				<p>Convert an old CRT from raster graphics to vector graphics. Implement Spacewar on an arduino/raspberry pi and use the CRT as the display</p>
+            <p>I work with a team that attempted this at a hackathon, details <a href="https://devpost.com/software/space-war">here.</a></p>
+            <p>Spacewar is one of the oldest video games. Info about it <a href="https://en.wikipedia.org/wiki/Spacewar!">here</a></p>
+            <p>CAUTION! CRT displays are dangerous and can kill you half a dozen different ways. Don't open them unless you know what you're doing!</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Lunch with a pro</h3></a>
+			<h3>Better delete history</h3>
 			<div class="copy">
-				<p></p>
+				<p>After deleting your history, it generates a bunch of fake, innocent looking history. Few things are more suspicious than a completely empty browser history.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>cbord cardinal cash chart</h3></a>
+			<h3>Suing machine</h3>
 			<div class="copy">
-				<p></p>
+				<p>Generates random law suit documents using basic AI. Allows you to pronounce sewing machine wrong with impunity.</p>
 			</div>
 		</li>
 		<li>
-			<a href="drip/"><h3>Better delete history</h3></a>
+			<h3>Better motion lights</h3>
 			<div class="copy">
-				<p></p>
+				<p>Motion lights that don't turn off when they stop seeing motion, they turn off when they sense motion somewhere else. Only really works if there's only one person though... This idea still needs work</p>
 			</div>
 		</li>
-		<li>
-			<a href="drip/"><h3>Desktop cup holder (w/ drink dispenser?)</h3></a>
-			<div class="copy">
-				<p></p>
-			</div>
-		</li>
-		<li>
-			<a href="drip/"><h3>Ad detect radio</h3></a>
-			<div class="copy">
-				<p></p>
-			</div>
-		</li>
-		<li>
-			<a href="drip/"><h3>Suing machine</h3></a>
-			<div class="copy">
-				<p></p>
-			</div>
-		</li>
-		<li>
-			<a href="drip/"><h3>Better motion lights</h3></a>
-			<div class="copy">
-				<p></p>
-			</div>
-		</li>
-		<li>
-			<a href="drip/"><h3>"Am I wasting my time" website (yes)</h3></a>
-			<div class="copy">
-				<p></p>
-			</div>
-		</li
 	</ul>
 </body>
 </html>
