0ba2bb0c530e8c9a96ab31b912cf1a8b4c7e20dd
Andrew Combs <andrewcombs13@gmail.com>
2018-03-27 17:39:50 -0400

    Fix another semicolon issue in resume.

--- resume/resumeHeader.php
+++ resume/resumeHeader.php
@@ -3,10 +3,10 @@
     <div class="container">
         <h1>Andrew Combs</h1>
         <ul>
-            <li><a href="resume.php" <?php if ($page == "resume") { echo("class='active'") } ?>>Resume</a></li
+            <li><a href="resume.php" <?php if ($page == "resume") { echo("class='active'"); } ?>>Resume</a></li
             ><li><a href="resume.pdf">Download PDF</a></li
-            ><li><a href="portfolio.php" <?php if ($page == "portfolio") { echo("class='active'") } ?>>Portfolio</a></li
-            ><li class="last-item"><a href="contact.php" <?php if ($page == "contact") { echo("class='active'") } ?>>Contact</a></li>
+            ><li><a href="portfolio.php" <?php if ($page == "portfolio") { echo("class='active'"); } ?>>Portfolio</a></li
+            ><li class="last-item"><a href="contact.php" <?php if ($page == "contact") { echo("class='active'"); } ?>>Contact</a></li>
         </ul>
     </div>
 </div>
