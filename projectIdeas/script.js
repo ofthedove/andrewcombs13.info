88828a5946f20d4fc6b7278a0eeffba8d8b07998
Andrew Combs <ajcomb01@louisville.edu>
2020-03-13 10:20:39 -0400

    Switch to loading ideas list via javascript from a json file. No new functionality yet.

---
+++ projectIdeas/script.js
@@ -0,0 +1,36 @@
+const ideasList = document.querySelector('ul');
+
+const requestUrl = "https://andrewcombs13.com/projectIdeas/Ideas.json";
+let request = new XMLHttpRequest();
+request.open('GET', requestUrl);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    const response = request.response;
+    showList(response);
+}
+
+function showList(jsonObj) {
+    const ideas = jsonObj['ideas'];
+
+    for(let i = 0; i < ideas.length; i++) {
+        const listItem = document.createElement('li');
+        const header = document.createElement('h3');
+        const descDiv = document.createElement('div');
+
+        header.textContent = ideas[i].name;
+
+        const descParas = ideas[i].description;
+        for(let j = 0; j < descParas.length; j++) {
+            const p = document.createElement('p');
+            p.textContent = descParas[j];
+            descDiv.appendChild(p);
+        }
+
+        listItem.appendChild(header);
+        listItem.appendChild(descDiv);
+
+        ideasList.appendChild(listItem);
+    }
+}
